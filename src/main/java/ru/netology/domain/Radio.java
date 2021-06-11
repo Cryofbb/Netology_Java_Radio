fa4d933a5c2e03f49b11c23e2e38268461fea71e
Cryofbb <78804095+Cryofbb@users.noreply.github.com>
2021-06-11 17:27:29 +0300

    Code revision

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -10,15 +10,15 @@ public class Radio {
 
     public void setMaxChannel(int maxChannel) {
         this.maxChannel = maxChannel;
-    } // Изменение max канала
+    }
 
     public void setMinChannel(int minChannel) {
         this.minChannel = minChannel;
-    } // Изменение min канала
+    }
 
     public int getChannel() {
         return currentChannel;
-    } // Текущий канал
+    }
 
     public void setChannel(int currentChannel) {
         if (currentChannel > maxChannel) {
@@ -28,41 +28,37 @@ public class Radio {
             currentChannel = minChannel;
         }
         this.currentChannel = currentChannel;
-    }// Произвольное изменение текущего канала. Если больше min/max то нет изменений
+    }
 
     public int increaseChannel() {
-        if (currentChannel < maxChannel) {
+        if(currentChannel < maxChannel) {
             currentChannel = currentChannel + 1;
-        }
-        if (currentChannel == maxChannel) {
+        } else if (currentChannel == maxChannel) {
             currentChannel = minChannel;
         }
         return currentChannel;
-    } // Увеличение номера канала
+    }
 
     public int decreaseChannel() {
         if (currentChannel > minChannel) {
             currentChannel = currentChannel - 1;
-        }
-        if (currentChannel == minChannel) {
+        } else if (currentChannel == minChannel) {
             currentChannel = maxChannel;
         }
         return currentChannel;
-    } // Уменьшение номера канала
-
-    // Громкость
+    }
 
     public void setMaxVolume(int maxVolume) {
         this.maxVolume = maxVolume;
-    } // Изменение max громкости
+    }
 
     public void setMinVolume(int minVolume) {
         this.minVolume = minVolume;
-    } // Изменение min громкости
+    }
 
     public int getVolume() {
         return currentVolume;
-    } // Текущая громкость
+    }
 
     public void setVolume(int currentVolume) {
         if (currentVolume > maxVolume) {
@@ -72,7 +68,7 @@ public class Radio {
             currentVolume = minVolume;
         }
         this.currentVolume = currentVolume;
-    }// Произвольное изменение громкости. Если больше min/max то нет изменений
+    }
 
     public int increaseVolume() {
         if (currentVolume < maxVolume) {
@@ -82,7 +78,7 @@ public class Radio {
             return currentVolume;
         }
         return currentVolume;
-    } // Увеличение громкости
+    }
 
     public int decreaseVolume() {
         if (currentVolume > minVolume) {
@@ -92,5 +88,5 @@ public class Radio {
             return currentVolume;
         }
         return currentVolume;
-    } // Уменьшение громкости
+    }
 }
--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -13,7 +13,7 @@ public class RadioTest {
         radio.setMinChannel(0);
         radio.setChannel(1);
         assertEquals(1, radio.getChannel());
-    } // Изменение канала в пределах min-max
+    }
 
     @Test
     void changeChannelMoreMax() {
@@ -21,7 +21,7 @@ public class RadioTest {
         radio.setMinChannel(0);
         radio.setChannel(11);
         assertEquals(9, radio.getChannel());
-    } // Изменение канала больше max
+    }
 
     @Test
     void changeChannelLessMin() {
@@ -29,23 +29,39 @@ public class RadioTest {
         radio.setMinChannel(1);
         radio.setChannel(0);
         assertEquals(1, radio.getChannel());
-    }// Изменение канала меньше min
+    }
 
     @Test
     void increaseChannel() {
         radio.setMaxChannel(9);
         radio.setMinChannel(0);
-        radio.setChannel(4);
-        assertEquals(5, radio.increaseChannel());
-    }// Увеличение канала на 1 пункт
+        radio.setChannel(8);
+        assertEquals(9, radio.increaseChannel());
+    }
+
+    @Test
+    void increaseChannelOverMax() {
+        radio.setMaxChannel(9);
+        radio.setMinChannel(0);
+        radio.setChannel(9);
+        assertEquals(0, radio.increaseChannel());
+    }
 
     @Test
     void decreaseChannel() {
         radio.setMaxChannel(9);
         radio.setMinChannel(0);
-        radio.setChannel(4);
-        assertEquals(3, radio.decreaseChannel());
-    }// Уменьшение канала на 1 пункт
+        radio.setChannel(1);
+        assertEquals(0, radio.decreaseChannel());
+    }
+
+    @Test
+    void decreaseChannelLessMin() {
+        radio.setMaxChannel(9);
+        radio.setMinChannel(0);
+        radio.setChannel(0);
+        assertEquals(9, radio.decreaseChannel());
+    }
 
     @Test
     void increaseChannelOverLimit() {
@@ -53,7 +69,7 @@ public class RadioTest {
         radio.setMinChannel(0);
         radio.setChannel(9);
         assertEquals(0, radio.increaseChannel());
-    }// Увеличение канала выше max
+    }
 
     @Test
     void decreaseChannelOverLimit() {
@@ -61,7 +77,7 @@ public class RadioTest {
         radio.setMinChannel(0);
         radio.setChannel(0);
         assertEquals(9, radio.decreaseChannel());
-    }// Уменьшение канала ниже min
+    }
 
     @Test
     void changeVolume() {
@@ -69,7 +85,7 @@ public class RadioTest {
         radio.setMinVolume(0);
         radio.setVolume(1);
         assertEquals(1, radio.getVolume());
-    }// Изменение громкости в пределах min-max
+    }
 
     @Test
     void changeVolumeMoreMax() {
@@ -77,7 +93,7 @@ public class RadioTest {
         radio.setMinVolume(0);
         radio.setVolume(15);
         assertEquals(10, radio.getVolume());
-    }// Изменение громкости больше Max
+    }
 
     @Test
     void changeVolumeLessMin() {
@@ -85,7 +101,7 @@ public class RadioTest {
         radio.setMinVolume(5);
         radio.setVolume(1);
         assertEquals(5, radio.getVolume());
-    }// Изменение громкости меньше min
+    }
 
     @Test
     void increaseVolume() {
@@ -93,7 +109,7 @@ public class RadioTest {
         radio.setMinVolume(0);
         radio.setVolume(4);
         assertEquals(5, radio.increaseVolume());
-    }// Увеличение громкости на 1 пункт
+    }
 
     @Test
     void decreaseVolume() {
@@ -101,7 +117,7 @@ public class RadioTest {
         radio.setMinVolume(0);
         radio.setVolume(4);
         assertEquals(3, radio.decreaseVolume());
-    }// Уменьшение громкости на 1 пункт
+    }
 
     @Test
     void increaseVolumeOverLimit() {
@@ -109,7 +125,7 @@ public class RadioTest {
         radio.setMinVolume(0);
         radio.setVolume(11);
         assertEquals(10, radio.increaseVolume());
-    }// Увеличение громкости выше max
+    }
 
     @Test
     void decreaseVolumeOverLimit() {
@@ -117,5 +133,5 @@ public class RadioTest {
         radio.setMinVolume(0);
         radio.setVolume(0);
         assertEquals(0, radio.decreaseVolume());
-    }// Уменьшение громкости ниже min
+    }
 }
